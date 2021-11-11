97d9cf86eba24e761b66b8cedc0700525c6be101
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2021-11-11 21:44:06 +0900

    updated App.js and Index.js and css files

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App ">
-      <div className="Weather container">
+      <div className="weatherData container">
         <h1>Weather App</h1>
         <h2>
           {" "}
